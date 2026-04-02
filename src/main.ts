81df4bce324842a5a4c51737df65cc7bfcdbcd43
Frsxk <fazaalbanna0410@gmail.com>
2026-04-03 06:00:57 +0700

    fix: No more CORS

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(cookieParser());
-  app.enableCors({
-    origin: process.env.CORS_ORIGIN || 'https://ais-ng.netlify.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
 
   app.useGlobalPipes(
     new ValidationPipe({
